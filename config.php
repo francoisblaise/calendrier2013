b18bdf921490d2bf587c8d358ea7eca990e78873
François BLAISE <francois.blaise@gmail.com>
2013-02-17 17:52:47 +0100

    Modif Config pour localhost

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 try{
-    $DB = new PDO('mysql:host=localhost;dbname=tuto','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+    $DB = new PDO('mysql:host=localhost;dbname=calendrier','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
 }
 catch(PDOException $e){
-    echo 'Base de donnée en vacance';
+    echo 'Base de donnée en vacances';
     exit();
 }
 ?>
